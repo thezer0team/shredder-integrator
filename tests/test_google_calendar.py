6a75fecc0a5feeeb53e1195ed618a2ffa207dd0e
Kevin Dienst <kevin.dienst@gmail.com>
2019-07-26 15:36:50 -0500

    Add test for google_calendar.py
    This is simple boilerplate for future test cases

---
+++ tests/test_google_calendar.py
@@ -0,0 +1,12 @@
+from google_calendar import grab_calendars
+from google_calendar import transform_events
+from google_calendar import grab_events
+from google_calendar import verify_platform_calendar
+from google_calendar import verify_user_appcalID
+from google_calendar import verify_user_exists
+from google_calendar import request_new_appcalID
+from google_calendar import request_new_userID
+from google_calendar import post_events
+
+def test_post_events():
+    assert True
