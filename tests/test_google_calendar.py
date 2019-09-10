d7cb4eddf8e4e612848621f0b25f3ce4984800b8
Kevin Dienst <axi0m@users.noreply.github.com>
2019-09-10 11:27:02 -0500

    Add pytest-icdiff

--- tests/test_google_calendar.py
+++ tests/test_google_calendar.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from google_calendar import grab_calendars
 from google_calendar import transform_events
 from google_calendar import grab_events
@@ -23,23 +22,15 @@ test_dict = {
         }
 }
 normalized_result = {
-        "summary": "Test Summary",
-        "description": "Test Description",
-        "start": "2019-08-23",
-        "end": "2019-09-01",
-        "status": "Busy",
         "attendees": "test.user@gmail.com",
-}
-
+        "status": "Busy",
+        "end": "2019-09-01",
+        "start": "2019-08-23",
+        "description": "Test Description",
+        "summary": "Test Summary",
+        }
 
 def test_transform_events():
     ''' Test data normalizer '''
     result = transform_events(test_dict)
     assert result == normalized_result
-
-
-def test_transform_event_has_summary():
-    ''' Test that transform function has summary '''
-    result = transform_events(test_dict)
-    summary = result.get('summary')
-    assert summary
